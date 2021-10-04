d3c6f89c322765224ac8b08643f6937c33638061
Boris Sobolev <rafinad@outlook.com>
2021-10-04 16:54:09 +0300

    Initial Commit

---
+++ DotInversionAnimation/DotInversionAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DotInversionAnimationApp.swift
+//  DotInversionAnimation
+//
+//  Created by Boris Sobolev on 04.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct DotInversionAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DotInversionAnimation/View/Home.swift
@@ -0,0 +1,8 @@
+//
+//  Home.swift
+//  DotInversionAnimation
+//
+//  Created by Boris Sobolev on 04.10.2021.
+//
+
+import Foundation
